60115a89d57a473a6533e08796f71719df6536ea
EsaSalmik <esa.salmikangas@jamk.fi>
2016-01-13 19:57:26 +0200

    Add draw of the window and cross

--- IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava1/MainWindow.xaml.cs
@@ -3,6 +3,7 @@
 * This file is part of the IIO11300 course project.
 * Created: 12.1.2016 Modified: 13.1.2016
 * Authors: Tero ,Esa Salmikangas
+* luotu Testit@Kotikone-branchi 13.1.2016 klo 19:21
 */
 using System;
 using System.Collections.Generic;
@@ -31,38 +32,75 @@ namespace Tehtava1
       InitializeComponent();
     }
 
-        private void btnCalculate_Click(object sender, RoutedEventArgs e)
-        {
-            //TODO
-            try
-            {
-                double result;
-                result = BusinessLogicWindow.CalculatePerimeter(1, 1);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                //yield to an user that everything okay
-            }
-        }
+    private void btnCalculate_Click(object sender, RoutedEventArgs e)
+    {
+      //TODO
+      try
+      {
+        double result;
+        result = BusinessLogicWindow.CalculatePerimeter(1, 1);
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show(ex.Message);
+      }
+      finally
+      {
+        //yield to an user that everything okay
+      }
+    }
 
     private void btnClose_Click(object sender, RoutedEventArgs e)
     {
       Application.Current.Shutdown();
     }
+
+    private void btnDraw_Click(object sender, RoutedEventArgs e)
+    {
+      //TODO piirrä ikkuna oikeassa koossa MyRectangle-elementillä
+      //TODO tarkistukset että annetut arvot ok
+      double w = double.Parse(txtWidht.Text);
+      double h = double.Parse(txtHeight.Text);
+      if (w > 0 & w <= 1000 & h > 0 & h <= 1000)
+      {
+        //piirretään ikkuna
+        myRectangle.Width = w;
+        myRectangle.Height = h;
+        //keskelle
+        
+        if (chkAddCross.IsChecked.Value)
+        {
+          //piirretään pystyristikko
+          Rectangle rect = new Rectangle();
+          rect.StrokeThickness = 2.5;
+          rect.Stroke = new SolidColorBrush(Colors.Black);
+          rect.Width = 5;
+          rect.Height = h;
+          //TODO mihin paikkaan
+          myCanvas.Children.Add(rect);
+          Canvas.SetLeft(rect, (w / 2D) + 5 );
+          //piirretään vaakaristikko
+          Rectangle rect2 = new Rectangle();
+          rect2.StrokeThickness = 2.5;
+          rect2.Stroke = new SolidColorBrush(Colors.Black);
+          rect2.Width = w;
+          rect2.Height = 5;
+          //TODO mihin paikkaan
+          myCanvas.Children.Add(rect2);
+          Canvas.SetTop(rect2,(h / 2D) + 5);
+        }
+      }
+    }
   }
 
   public class BusinessLogicWindow
-    {
+  {
     /// <summary>
     /// CalculatePerimeter calculates the perimeter of a window
     /// </summary>
     public static double CalculatePerimeter(double widht, double height)
-        {
-            throw new System.NotImplementedException();
-        }
+    {
+      throw new System.NotImplementedException();
     }
+  }
 }
