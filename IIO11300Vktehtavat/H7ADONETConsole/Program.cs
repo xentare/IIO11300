8c9963f55c2b3967aa3fb0e171e36cdf9bf3ad76
xentare <H4113@student.jamk.fi>
2016-03-10 11:11:41 +0200

    Tuntiharjoituksia

---
+++ IIO11300Vktehtavat/H7ADONETConsole/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H7ADONETConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string connStr = H7ADONETConsole.Properties.Settings.Default.Tietokanta;
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT id, asioid, firstname, lastname, date FROM presences", conn);
+                    SqlCommand cmd2 = new SqlCommand("DELETE FROM presences WHERE id = 334", conn);
+                    SqlCommand cmd3 = new SqlCommand("UPDATE presences SET date = '2016-01-28' WHERE id = 333", conn);
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    if (rdr.HasRows)
+                    {
+                        while (rdr.Read())
+                        {
+                            Console.WriteLine("Läsnäolosi {0} {1} {2} {3} {4}",rdr.GetInt16(0),rdr.GetString(1),rdr.GetString(2), rdr.GetString(3), rdr.GetDateTime(4));
+                        }
+
+                    }
+                    rdr.Close();
+                    conn.Close();
+                    Console.WriteLine("Tietokantayhteys suljettu onnistuneesti.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/H8ADONETWpf/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H8ADONETWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetData();
+        }
+
+        private void GetData()
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(H8ADONETWpf.Properties.Settings.Default.Tietokanta))
+                {
+                    string sql = "SELECT id, asioid, firstname, lastname, date FROM presences";
+                    SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                    DataTable dt = new DataTable("presences");
+                    da.Fill(dt);
+
+                    myGrid.DataContext = dt;
+                    conn.Close();
+                }    
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
